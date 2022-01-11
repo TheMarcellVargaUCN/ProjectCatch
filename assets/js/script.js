951cbbc2cd213e62bb83de8cb2422962aa1c8888
TheMarcellVargaUCN <74178025+TheMarcellVargaUCN@users.noreply.github.com>
2022-01-11 22:32:04 +0100

    Responsivity make over 1.0 + Mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+const hamburger = document.querySelector(".navbar-hamburger");
+const navMenu = document.querySelector(".navbar-links");
+
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("translate-x-0");
+    navMenu.classList.toggle("translate-x-0");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
 function notifications() {
     let bell = document.getElementById('bell');
     
